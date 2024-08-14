78938dff7648b1aa8ba8a56c04e8d917db167b74
U Siu Ki <usluklucc@gmail.com>
2024-08-14 16:20:15 +0800

    Add files via upload

---
+++ boilerplate-mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def calculate(list):
+
+    Num = np.array([
+        [list[0],list[1],list[2]],
+        [list[3],list[4],list[5]],
+        [list[6],list[7],list[8]],
+    ])
+
+
+    return calculations
